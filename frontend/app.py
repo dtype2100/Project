50d1e4ae454f05b95dc3458ec6bbca486af9723d
jin <datatype2100@gmail.com>
2024-11-25 20:04:20 +0900

    add a session to the dashboard app

--- frontend/app.py
+++ frontend/app.py
@@ -1,20 +1,26 @@
 import streamlit as st
-import requests
 import pandas as pd
 
 # Streamlit UI
 st.title("Streamlit to FastAPI Workflow")
 
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
-    st.write("Uploaded Data:")
-    st.write(df)
-    
+uploaded_data = st.file_uploader("Upload a CSV file", type=["csv"])
 
-    selected_columns = st.multiselect('select columns', [cols for cols in df.columns])
 
+if 'df' not in st.session_state:
+    st.session_state['df'] = None
+
+if uploaded_data:
+    st.session_state['df'] = pd.read_csv(uploaded_data)
+    st.success('Data uploaded successfully!')
+
+df = st.session_state['df']
+
+if df is not None:
+    selected_columns = st.multiselect("Select columns", options=df.columns)
     if selected_columns:
-        st.subheader("selected dataframe")
-        st.write(df[selected_columns])
-
+        filtered_df = df[selected_columns]
+        st.subheader("Filtered DataFrame")
+        st.write(filtered_df)
+else:
+    st.warning("Please upload a CSV file to proceed.")
