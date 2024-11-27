423720f2d10bc1debf17d56d6715b30f981261c7
jin <datatype2100@gmail.com>
2024-11-28 00:39:53 +0900

    add services -> prepro.py -> prepro_all_columns

--- backend/router/v1/prepro_router.py
+++ backend/router/v1/prepro_router.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Request
+from services.prepro import Preprocessing
 import pandas as pd
 
 router = APIRouter(
     prefix='/prepro_router',
     tags='/prepro_router'
 )
+
+pc = Preprocessing()
 
 @router.get('/prepro_sample_df')
 async def prepro_sample_df(request: Request):
@@ -21,17 +24,14 @@ async def prepro_sample_df(request: Request):
     return df
 
 @router.get('/prepro_all_df')
-async def prepro_all_df(request: Request):
+async def prepro_all_df(request: Request) -> pd.DataFrame:
 
     result = await request.json()
 
     df_data = result['df_data']
     payload = result['payload']
 
-    df = pd.read_json(df_data)
-    
-    
-    # 데이터 전처리 적용 예정
+    df = pc.prepro_all_columns(df_data=df_data, payload=payload)
 
     return df
 
--- backend/services/prepro.py
+++ backend/services/prepro.py
@@ -1,10 +1,34 @@
 import pandas as pd
+from typing import Union, Dict, Any
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 class Preprocessing:
     def __init__(self):
         pass
 
-    def apply_payload(self, df_data, payload):
-        df = pd.read_json(df_data)
+    def prepro_all_columns(
+            self, 
+            df_data: Union[str, Dict[str, Any]], 
+            payload: Dict[str, str]
+            ) -> pd.DataFrame:
         
-        return None
+        df = pd.read_json(df_data)
+        for _, var in payload.items():
+            if var == 'all_col_dropna':
+                df.dropna(inplace=True)
+
+            elif var == 'all_col_bfill':
+                df.fillna(method='bfill', inplace=True)
+
+            elif var == 'all_col_ffill':
+                df.fillna(method='ffill', inplace=True)
+
+            elif var == 'minmax_scale':  
+                scaler = MinMaxScaler()
+                df = scaler.fit_transform(df)
+
+            elif var == 'standard_scale':
+                scaler = StandardScaler()
+                df = scaler.fit_transform(df)
+
+        return df
